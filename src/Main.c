ce66c68a798a770531800ed34f28c01d3514767c
SamuelEscalera <samuel.escalera.9413603@usalesiana.edu.bo>
2024-06-26 15:04:15 -0400

    Server init

--- src/Main.c
+++ src/Main.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <signal.h>
+
+#define MAX_CONNECTIONS 30
+
+int server_socket;
+
+int main(int argc, char *argv[]) {
+    
+    if(argc != 2){
+        fprintf(stderr, "Usage: %s port \n", argv[0]);
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
 
 
-int main() {
-    printf ("Hello world\n");
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror( "Socket creation failed");
+        return 1;
+    }
 
-    return 0;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        perror("Bind failed");
+        close(server_socket);
+        return 1;
+    } 
+
+    if (listen(server_socket, MAX_CONNECTIONS) < 0) {
+        perror("Listen failed");
+        close(server_socket);
+        return 1;
+    }
+
+    printf("Server listening on port %d\n", port);
 }
